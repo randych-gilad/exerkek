70b0a6e5cb64ca11ea6c98ad8a72e019f963b0b5
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-18 00:16:59 +0200

    better decode with atom keys

--- lib/exerkek.ex
+++ lib/exerkek.ex
@@ -1,19 +1,30 @@
 defmodule Exerkek do
   @api_url "https://api.exercism.io/v2"
 
-  defp decode_to_only do
-    with {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.get(@api_url <> "/tracks") do
+  defp filter_elixir_track(parseable) do
+    parseable
+    |> Jason.decode!(keys: :atoms)
+    |> Map.get(:tracks)
+    |> Enum.filter(fn map -> map.title == "Elixir" end)
+  end
+
+  def decode_to_only(parseable) when is_binary(parseable) do
+    parseable
+    |> filter_elixir_track()
+  end
+
+  def decode_to_only(parseable) do
+    with {:ok, %HTTPoison.Response{body: body}} <- parseable do
       body
-      |> Jason.decode!()
-      |> Map.get("tracks")
+      |> filter_elixir_track()
     else
-      error -> error
+      error -> {:error, error}
     end
   end
 
   def tracklist() do
-    decode_to_only()
-    |> IO.inspect()
+    decode_to_only(HTTPoison.get(@api_url <> "/tracks"))
+    # |> IO.inspect()
   end
 
   def main(_args) do
