34fafa482ca26f7c32dcb2074ba05a3e463f88eb
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-17 23:08:26 +0200

    first actually useful test

--- test/exerkek_test.exs
+++ test/exerkek_test.exs
@@ -2,7 +2,44 @@ defmodule ExerkekTest do
   use ExUnit.Case
   doctest Exerkek
 
-  test "greets the world" do
-    assert Exerkek.hello() == :world
+  @elixir_track %{
+    "course" => true,
+    "icon_url" => "https://assets.exercism.org/tracks/elixir.svg",
+    "is_new" => false,
+    "last_touched_at" => nil,
+    "links" => %{
+      "concepts" => "https://exercism.org/tracks/elixir/concepts",
+      "exercises" => "https://exercism.org/tracks/elixir/exercises",
+      "self" => "https://exercism.org/tracks/elixir"
+    },
+    "num_concepts" => 57,
+    "num_exercises" => 159,
+    "slug" => "elixir",
+    "tags" => [
+      "Compiled",
+      "Functional",
+      "Linux",
+      "macOS",
+      "Windows",
+      "BEAM (Erlang)",
+      "Dynamic",
+      "Strong",
+      "Backends",
+      "Embedded systems",
+      "Financial systems",
+      "Web development"
+    ],
+    "title" => "Elixir",
+    "web_url" => "https://exercism.org/tracks/elixir"
+  }
+
+  test "able to get only Elixir track information" do
+    with tracks_json <-
+           File.read!("test/tracks.json")
+           |> Jason.decode!()
+           |> Map.get("tracks")
+           |> Enum.filter(fn map -> map["title"] == "Elixir" end) do
+      assert hd(tracks_json) == @elixir_track
+    end
   end
 end
