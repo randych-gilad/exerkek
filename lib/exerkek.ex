7a2af27e29497b3c06aca93e9015355639960455
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-17 23:08:38 +0200

    shuffle stuff around for now

--- lib/exerkek.ex
+++ lib/exerkek.ex
@@ -1,16 +1,19 @@
 defmodule Exerkek do
   @api_url "https://api.exercism.io/v2"
 
-  def tracklist() do
+  defp decode_to_only() do
     with {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.get(@api_url <> "/tracks") do
       body
       |> Jason.decode!()
       |> Map.get("tracks")
-      |> Enum.filter(fn map -> map["title"] == "Elixir" end)
-      |> IO.inspect()
     else
       error -> error
     end
+  end
+
+  def tracklist() do
+    decode_to_only()
+    |> IO.inspect()
   end
 
   def main(_args) do
